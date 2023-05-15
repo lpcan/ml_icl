bec798ebed78792f85307aea2fbd5c498a88c262
Louisa Canepa <l.canepa@unsw.edu.au>
2023-05-15 15:49:53 +1000

    Masking out non member galaxies

--- measure_sb_cut/scripts/measure_icl.py
+++ measure_sb_cut/scripts/measure_icl.py
@@ -17,6 +17,10 @@ from scipy.interpolate import CloughTocher2DInterpolator
 import sys
 import tqdm
 
+from skimage.morphology import binary_opening
+import cv2
+from astropy.table import join
+
 def background_estimate(cutout, z, cosmo, mask=None):
     """
     Returns an estimate of the 2D background of `cutout`. The background is 
@@ -82,6 +86,41 @@ def create_circular_mask(z, img, cosmo):
 
     return mask
 
+def segment(thresholded_img):
+    # Erode and dilate the mask to get rid of the specks
+    obj_mask_open = binary_opening(thresholded_img, np.ones((5,5)))
+    binary_img = np.array(obj_mask_open, dtype=np.uint8)
+
+    # Segment the image using connected components method
+    _, labels = cv2.connectedComponents(binary_img, connectivity=8)
+    return labels
+
+def get_member_locs(idx, merged, cutout_shape):
+    # Get cluster location
+    cluster_ra = merged[merged['ID'] == idx]['RA_cl'][0]
+    cluster_dec = merged[merged['ID'] == idx]['Dec_cl'][0]
+    centre_px = (cutout_shape[0] // 2, cutout_shape[1] // 2)
+
+    # Get the cluster members
+    c_members = merged[merged['ID'] == idx]
+    ras = c_members['RA']
+    decs = c_members['Dec']
+    
+    # Get offsets in degrees and convert to pixels
+    ra_offsets = ras - cluster_ra
+    dec_offsets = decs - cluster_dec
+    x_offsets = ra_offsets * 3600 / 0.168
+    y_offsets = dec_offsets * 3600 / 0.168
+
+    # Get pixel locations
+    centre = (cutout_shape[1] // 2, cutout_shape[0] // 2)
+    x_locs_all = centre[0] - x_offsets # ra decreases from left to right
+    y_locs_all = centre[1] + y_offsets
+    x_locs = x_locs_all[(x_locs_all >= 0) & (x_locs_all < cutout_shape[1]) & (y_locs_all >= 0) & (y_locs_all < cutout_shape[0])]
+    y_locs = y_locs_all[(x_locs_all >= 0) & (x_locs_all < cutout_shape[1]) & (y_locs_all >= 0) & (y_locs_all < cutout_shape[0])]
+    
+    return (x_locs, y_locs)
+
 def counts2sb(counts, z):
     return 2.5 * np.log10(63095734448.0194 / counts) + 5 * np.log10(0.168) - 10 * np.log10(1+z)
 
@@ -93,9 +132,10 @@ def calc_icl_frac(args):
     Calculate the ratio of light below the threshold to the total light in the 
     image to get a rough estimate of the icl fraction.
     """
-    keys, length, zs = args
+    keys, length, zs, merged = args
+
     base_path = os.path.dirname(__file__)
-    cutouts = h5py.File(base_path + '/../../cutouts_550.hdf') #'/../processed/cutouts.hdf')
+    cutouts = h5py.File(base_path + '/../cutouts_550.hdf') #'/../processed/cutouts.hdf')
 
     # Find the shared memory and create a numpy array interface
     shmem = SharedMemory(name=f'iclbuf', create=False)
@@ -131,16 +171,22 @@ def calc_icl_frac(args):
         bkg = background_estimate(cutout, zs[key], cosmo, mask=bad_mask)
         bkg_subtracted = cutout - bkg
 
+        # Convert image from counts to surface brightness
+        np.seterr(invalid='ignore', divide='ignore')
+        sb_img = counts2sb(bkg_subtracted, 0)
+
+        # Flag the non member galaxies
+        seg_threshold = 26 + 10 * np.log10(1 + zs[int(key)])
+        mask = sb_img > seg_threshold
+        obj_mask = ~(mask + bad_mask + np.isnan(sb_img))
+        labels = segment(obj_mask)
+        x_locs, y_locs = get_member_locs(int(key), merged, cutout.shape)
+        c_members = labels[y_locs.astype(int), x_locs.astype(int)]
+        member_mask = np.isin(labels, c_members) | (labels == 0) # also make sure to get the background in
+
         # Calculate surface brightness limit (from Cristina's code (Roman+20))
         _, _, stddev = sigma_clipped_stats(bkg_subtracted, mask=bad_mask)
         sb_lim = -2.5 * np.log10(3*stddev/(0.168 * 10)) + 2.5 * np.log10(63095734448.0194)
-
-        # Mask the image
-        masked_img = bkg_subtracted * circ_mask
-
-        # Convert image from counts to surface brightness
-        np.seterr(invalid='ignore', divide='ignore')
-        sb_img = counts2sb(masked_img, 0)
 
         # Mask out the values below surface brightness limit
         sb_img[sb_img >= sb_lim] = np.nan
@@ -152,15 +198,15 @@ def calc_icl_frac(args):
         # Convert the SB image back to counts
         counts_img = sb2counts(sb_img)
 
-        weighted_img = counts_img * ~bad_mask
+        masked_img = counts_img * ~bad_mask * member_mask * circ_mask
 
-        fracs[0,key] = np.nansum(weighted_img * mask)
-        fracs[1,key] = np.nansum(weighted_img)
+        fracs[0,key] = np.nansum(masked_img * mask)
+        fracs[1,key] = np.nansum(masked_img)
         fracs[2,key] = fracs[0,key] / fracs[1,key]
 
     return
 
-def calc_icl_frac_parallel(cutouts, zs):
+def calc_icl_frac_parallel(cutouts, zs, members):
     """
     Use multiprocessing to divide the cutouts among available cores and 
     calculate the ICL fractions.
@@ -171,7 +217,7 @@ def calc_icl_frac_parallel(cutouts, zs):
     # Divide the keys up into 20 
     jobs = np.array_split(np.arange(len(cutouts.keys())), 20)
     length = len(cutouts.keys()) * 3
-    args = [(j, length, zs) for j in jobs]
+    args = [(j, length, zs, merged) for j in jobs]
 
     exit = False
     try:
@@ -212,12 +258,20 @@ if __name__ == '__main__':
     base_path = os.path.dirname(__file__)
 
     # Load the cutouts and the corresponding redshifts
-    cutouts = h5py.File(base_path + '/../../cutouts_550.hdf') #'/../processed/cutouts.hdf')
-    tbl = ascii.read(base_path + '/../../dud_only.tbl', #'/../processed/camira_final.tbl', 
-                    names=['ID', 'Name', 'RA', 'Dec', 'z', 'Richness', 'BCG z'])
+    cutouts = h5py.File(base_path + '/../cutouts_550.hdf') #'/../processed/cutouts.hdf')
+    tbl = ascii.read(base_path + '/../../data/processed/camira_final.tbl', #'/../processed/camira_final.tbl', 
+                    names=['ID', 'Name', 'RA [deg]', 'Dec [deg]', 'z', 'Richness', 'BCG z'])
     zs = tbl['z']
+    
+    # Load the cluster member catalogue
+    members = ascii.read(base_path + '/../../data/raw/camira_s20a_dud_member.dat', 
+                        names=['RA_cl', 'Dec_cl', 'Richness', 'z_cl', 'RA', 'Dec', 'M', 'w'])
 
-    fracs = calc_icl_frac_parallel(cutouts, zs)
+    # Match this catalogue to the cluster catalogue
+    merged = join(members, tbl, keys_left=['RA_cl', 'Dec_cl'], keys_right=['RA [deg]', 'Dec [deg]'])
+    merged = merged['ID', 'Name', 'RA_cl', 'Dec_cl', 'z_cl', 'RA', 'Dec']
+
+    fracs = calc_icl_frac_parallel(cutouts, zs, merged)
 
     ranked = np.argsort(fracs[2])
     mask = ~np.isnan(fracs[2][ranked]) # Don't include nans in the top 5
