6b977e73c6d8bd18c21ac8c0a480b62d6fd41992
Louisa Canepa <l.canepa@unsw.edu.au>
2023-06-13 15:57:19 +1000

    updating RandomResizedCrop to do a smarter zoom

--- augmentations.py
+++ augmentations.py
@@ -14,21 +14,35 @@ class RandomResizedCrop(layers.Layer):
     Crop is offset with a max of `jitter_max`.
     """
 
-    def __init__(self, scale, ratio, prob_ratio_change, jitter_max):
+    def __init__(self, ratio, prob_ratio_change, jitter_max):
         super().__init__()
-        self.scale = scale
         self.ratio = ratio
         self.prob_ratio_change = prob_ratio_change
         self.jitter_max = jitter_max
 
     def call(self, images):
-        batch_size = tf.shape(images)[0]
-        height = tf.shape(images)[1]
-        width = tf.shape(images)[2]
-        
-        # Draw uniformly random scales
-        random_scales = tf.random.uniform((batch_size,), self.scale[0], self.scale[1])
-        
+        batch_size, height, width, _ = tf.shape(images)
+
+        # Determine the maximum allowed zoom amount
+        mid = [height // 2, width // 2]
+
+        norths = tf.reverse(images[:, 0:mid[0], mid[1], :], axis=[1])
+        souths = images[:, mid[0]:, mid[1], :]
+        wests = tf.reverse(images[:, mid[0], 0:mid[1], :], axis=[1])
+        easts = images[:, mid[0], mid[1]:]
+
+        max_widths = tf.ones(batch_size, dtype=tf.dtypes.int64) * 20 # Impose minimum width of 40x40 box
+
+        for lines in [norths, souths, wests, easts]:
+            widths = tf.squeeze(tf.math.argmax(lines < 0.006, axis=1)) # Find half widths of peaks
+            widths_fixed = tf.where(widths == 0, height // 2, widths) # If not found, default to normal size of image
+            max_widths = tf.math.maximum(max_widths, widths_fixed) # Find the new maximum widths
+
+        max_zoom = max_widths * 2 / height
+
+        # Draw uniformly random scales up to the maximum zoom amount
+        random_scales = tf.random.uniform((batch_size,), max_zoom, 1.)
+
         # Apply image stretching with probability `prob_ratio_change`
         random_ratios = tf.random.uniform((batch_size,), self.ratio[0], self.ratio[1]) # Draw uniformly distributed aspect ratios
         random_nums = tf.random.uniform((batch_size,), 0.0, 1.0)
@@ -86,13 +100,13 @@ class GaussianSmooth(layers.Layer):
         # Select the smoothed version with probability `self.prob`
         return tf.where(random_nums < self.prob, smoothed, images)
         
-def augmenter(input_shape, crop_scale=(0.5,1.0), crop_ratio=3/4, crop_prob=0.5,
+def augmenter(input_shape, crop_ratio=3/4, crop_prob=0.5,
               crop_jitter_max=0.1, smooth_prob=0.5):
     return keras.Sequential(
         [
             layers.Input(shape=input_shape),
             layers.RandomFlip(mode="horizontal_and_vertical"),
-            RandomResizedCrop(scale=crop_scale, ratio=(crop_ratio, 1/crop_ratio), prob_ratio_change=crop_prob, jitter_max=crop_jitter_max),
+            RandomResizedCrop(ratio=(crop_ratio, 1/crop_ratio), prob_ratio_change=crop_prob, jitter_max=crop_jitter_max),
             RandomGaussianNoise(stddev=0.017359),
             GaussianSmooth(prob=smooth_prob),
             # other augmentations? rotation, etc?
