48b67cc78bd87446bdf4e73ce026dc552341e877
Louisa Canepa <l.canepa@unsw.edu.au>
2023-07-12 14:46:10 +1000

    updated data generation

--- generate_train_data.py
+++ generate_train_data.py
@@ -1,6 +1,7 @@
 from astropy.convolution import Gaussian2DKernel, convolve
 from astropy.io import ascii
 from astropy.modeling.functional_models import Sersic2D
+from astropy.table import join
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,13 +13,47 @@ stddev = 0.017359
 def stretch(img):
     return np.arcsinh(np.clip(img, a_min=0.0, a_max=10.0) / stddev)
 
-cutouts = h5py.File('/srv/scratch/z5214005/hsc_icl/cutouts.hdf')
-zs = ascii.read('/srv/scratch/z5214005/camira_final.tbl')['z_cl']
+def get_member_locs(idx, merged, cutout_shape):
+    # Get cluster location
+    cluster_ra = merged[merged['ID'] == idx]['RA_cl'][0]
+    cluster_dec = merged[merged['ID'] == idx]['Dec_cl'][0]
 
-generated_data = h5py.File('/srv/scratch/z5214005/generated_data_400.hdf', 'w')
+    # Get the cluster members
+    c_members = merged[merged['ID'] == idx]
+    ras = c_members['RA']
+    decs = c_members['Dec']
+    
+    # Get offsets in degrees and convert to pixels
+    ra_offsets = ras - cluster_ra
+    dec_offsets = decs - cluster_dec
+    x_offsets = ra_offsets * 3600 / 0.168
+    y_offsets = dec_offsets * 3600 / 0.168
+
+    # Get pixel locations
+    centre = (cutout_shape[1] // 2, cutout_shape[0] // 2)
+    x_locs_all = centre[0] - x_offsets # ra decreases from left to right
+    y_locs_all = centre[1] + y_offsets
+    x_locs = x_locs_all[(x_locs_all >= 0) & (x_locs_all < cutout_shape[1]) & (y_locs_all >= 0) & (y_locs_all < cutout_shape[0])]
+    y_locs = y_locs_all[(x_locs_all >= 0) & (x_locs_all < cutout_shape[1]) & (y_locs_all >= 0) & (y_locs_all < cutout_shape[0])]
+    
+    return (x_locs, y_locs)
+
+cutouts = h5py.File('data/processed/cutouts.hdf')
+tbl = ascii.read('data/processed/camira_final.tbl')
+zs = tbl['z_cl']
+
+# Load the cluster member catalogue
+members = ascii.read('data/raw/camira_s20a_wide_member.dat', 
+                    names=['RA_cl', 'Dec_cl', 'Richness', 'z_cl', 'RA', 'Dec', 'M', 'w'])
+
+# Match this catalogue to the cluster catalogue
+merged = join(members, tbl, keys_left=['RA_cl', 'Dec_cl'], keys_right=['RA [deg]', 'Dec [deg]'])
+merged = merged['ID', 'Name', 'RA_cl', 'Dec_cl', 'z_cl_1', 'RA', 'Dec']
+
+generated_data = h5py.File('generated_data_400.hdf', 'w')
 fracs = []
 
-for num in range(126,526):
+for num in range(126,226):
     print(f'{num}', end='\r')
     cutout = cutouts[str(num)]['HDU0']['DATA']
 
@@ -38,6 +73,11 @@ for num in range(126,526):
     distances = scipy.spatial.distance.cdist([centre], np.argwhere(central_blob)[edges.vertices])[0] # Distances to edges of shape
     r_eff = np.median(distances) # Vague estimate of size of central blob
 
+    # Find the cluster members in the image
+    x_locs, y_locs = get_member_locs(num, merged, cutout.shape)
+    c_members = labels[y_locs.astype(int), x_locs.astype(int)]
+    member_mask = np.isin(labels, c_members) & labels.astype(bool)
+
     # Expand the masks of non central galaxies
     non_central_galaxies = bright_parts * ~central_blob 
     kernel = Gaussian2DKernel(7) # Large ish kernel
@@ -50,7 +90,8 @@ for num in range(126,526):
 
     # Generate some random parameters for the profile
     amplitude = threshold
-    n = np.random.randint(low=1, high=11)
+    # n = np.random.randint(low=1, high=11)
+    n = 1
     ellip = np.random.uniform(low=0, high=0.5)
     theta = np.random.uniform(low=0, high=2*np.pi)
 
@@ -72,11 +113,12 @@ for num in range(126,526):
     img = img_no_noise + noise
 
     # Calculate the new artificial ICL fraction
-    sb_limit = 27 + 10 * np.log10(1+z) # Calculate the sb limit
+    sb_limit = 28 + 10 * np.log10(1+z) # Calculate the sb limit
     limit = 10**(-0.4*(sb_limit - 2.5*np.log10(63095734448.0194) - 5.*np.log10(0.168))) # Convert to counts
     icl = np.sum(icl_img[icl_img > limit])
+
     # total = np.sum(img_no_noise) # Total brightness (without considering noise)
-    total = np.sum((cutout * central_blob) + icl_img)
+    total = np.sum((cutout * member_mask) + icl_img)
     # Add to file
     generated_data[f'{num}/HDU0/DATA'] = img
     generated_data[f'{num}/FRAC'] = icl / total
@@ -87,7 +129,7 @@ for num in range(126,526):
     # plt.subplot(131)
     # plt.imshow(stretch(icl_img))
     # plt.subplot(132)
-    # plt.imshow(stretch(img_no_noise))
+    # plt.imshow(stretch((cutout * member_mask) + icl_img))
     # plt.subplot(133)
     # plt.imshow(stretch(img))
     # plt.suptitle(str(icl / total))
@@ -95,7 +137,7 @@ for num in range(126,526):
 
     fracs.append(icl / total)
 
-plt.hist(fracs)
+plt.hist(fracs, 15)
 # plt.xlim(0,0.3)
 plt.xlabel('ICL fraction')
 plt.ylabel('Counts')
