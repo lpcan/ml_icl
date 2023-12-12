dd9cd1f5b4bb048eed2dd7bab911e7d513c89bda
Louisa Canepa <lcanepa3@gmail.com>
2023-12-12 14:04:45 +1100

    fix over/undersegmentation of central

--- generate_train_data.py
+++ generate_train_data.py
@@ -9,7 +9,7 @@ import scipy
 from skimage.morphology import binary_opening
 import skimage
 
-from photutils.segmentation import SourceFinder
+from photutils.segmentation import detect_sources
 from astropy.stats import sigma_clipped_stats, mad_std
 
 # Convenience function for plotting the images
@@ -42,40 +42,37 @@ def get_member_locs(idx, merged, cutout_shape):
     
     return (x_locs, y_locs)
 
-cutouts = h5py.File('/srv/scratch/z5214005/lrg_cutouts_resized.hdf')
-tbl = ascii.read('/srv/scratch/z5214005/lrgs_sampled.tbl')
-zs = tbl['z']
+def inject_icl(cutout_id, cutouts, z, seg_threshold=26):
+    
+    cutout = cutouts[str(cutout_id)]['HDU0']['DATA']
 
-# # Load the cluster member catalogue
-# members = ascii.read('data/raw/camira_s20a_wide_member.dat', 
-#                     names=['RA_cl', 'Dec_cl', 'Richness', 'z_cl', 'RA', 'Dec', 'M', 'w'])
+    sb_threshold = seg_threshold + 10 * np.log10(1+z) # Calculate mask threshold at this z
+    threshold = 10**(-0.4*(sb_threshold - 2.5*np.log10(63095734448.0194) - 5.*np.log10(0.168))) # Convert to counts
 
-# # Match this catalogue to the cluster catalogue
-# merged = join(members, tbl, keys_left=['RA_cl', 'Dec_cl'], keys_right=['RA [deg]', 'Dec [deg]'])
-# merged = merged['ID', 'Name', 'RA_cl', 'Dec_cl', 'z_cl_1', 'RA', 'Dec']
+    # Some convoluted stuff so the central blob is not over or undersegmented
+    labels = detect_sources(cutout, threshold, npixels=20).data
+    eroded = skimage.morphology.binary_erosion((labels > 0), np.ones((3,3)))
+    markers, _ = scipy.ndimage.label(eroded)
+    distance = scipy.ndimage.distance_transform_edt(labels > 0)
+    watershed_labels = skimage.segmentation.watershed(-distance, markers, mask=(labels > 0))
+    # Need to renumber watershed labels to prevent two different objects with same label
+    labels = np.where(watershed_labels > 0, watershed_labels + np.max(labels), labels)
 
-generated_data = h5py.File('/srv/scratch/mltidal/generated_data_experiment.hdf', 'w')
-fracs = []
-finder = SourceFinder(npixels=20, progress_bar=False, nlevels=8)
-
-for num in range(len(tbl)):
-    print(f'{num}', end='\r')
-    cutout_id = tbl['new_ids'][num]
-    cutout = cutouts[cutout_id]['HDU0']['DATA']
-
-    z = zs[num]
     sb_threshold = 26 + 10 * np.log10(1+z) # Calculate mask threshold at this z
     threshold = 10**(-0.4*(sb_threshold - 2.5*np.log10(63095734448.0194) - 5.*np.log10(0.168))) # Convert to counts
 
     # Figure out what to use as r_eff
     bright_parts = (cutout > threshold)
-    labels = finder(cutout, threshold).data
-    # labels, _ = scipy.ndimage.label(bright_parts) # Label each bright section
+
     centre = (cutout.shape[0] // 2, cutout.shape[1] // 2)
     central_blob = bright_parts * (labels == labels[centre[0], centre[1]])
     if np.sum(central_blob) == 0:
-        continue
+        return None
 
+    # Update estimation of central blob centroid
+    coords = np.where(central_blob == 1)
+    centre = (int(np.sum(coords[0]) / coords[0].shape), int(np.sum(coords[1]) / coords[1].shape))
+    
     # # Find the cluster members in the image
     # x_locs, y_locs = get_member_locs(num, merged, cutout.shape)
     # c_members = labels[y_locs.astype(int), x_locs.astype(int)]
@@ -90,12 +87,12 @@ for num in range(len(tbl)):
     # Put just the blurred edges into the original mask
     non_central_blurred = np.where(non_central_galaxies | central_blob, non_central_galaxies, non_central_blurred)
 
-    final_bright_parts = non_central_blurred + central_blob
+    final_bright_parts = non_central_galaxies + central_blob
 
     # Figure out what r_eff of the profile should be
     opened_blob = binary_opening(central_blob, np.ones((2,2)))
-    labelled_blob = skimage.measure.label(opened_blob)
-    opened_blob = opened_blob * (labelled_blob == labelled_blob[centre[0], centre[1]])
+    # labelled_blob = skimage.measure.label(opened_blob)
+    # opened_blob = opened_blob * (labelled_blob == labelled_blob[centre[0], centre[1]])
 
     edges = scipy.spatial.ConvexHull(np.argwhere(opened_blob)) # Convex hull of central blob
 
@@ -120,10 +117,11 @@ for num in range(len(tbl)):
         x,y = np.meshgrid(np.arange(cutout.shape[1]), np.arange(cutout.shape[0]))
 
         icl_img = np.clip(model(x,y), a_min=None, a_max=threshold)
-        icl_img = np.where(central_blob, 0, icl_img)
+        # icl_img = np.where(central_blob, 0, icl_img)
+        icl_img = np.where(final_bright_parts, 0, icl_img)
 
         # Calculate the new artificial ICL fraction
-        sb_limit = 28 + 10 * np.log10(1+z) # Calculate the sb limit
+        sb_limit = 30.2 + 10 * np.log10(1+z) # Calculate the sb limit
         limit = 10**(-0.4*(sb_limit - 2.5*np.log10(63095734448.0194) - 5.*np.log10(0.168))) # Convert to counts
         icl = np.sum(icl_img[icl_img > limit])
 
@@ -134,48 +132,68 @@ for num in range(len(tbl)):
         quantile -= 0.25
     
     # Generate the final image
+    # Add the ICL image to the non bright parts
+    bkg = cutout * ~final_bright_parts
+    # sb_limit = 29 + 10 * np.log10(1+z) # Calculate the sb limit
+    # limit = 10**(-0.4*(sb_limit - 2.5*np.log10(63095734448.0194) - 5.*np.log10(0.168))) # Convert to counts
+    # bkg = bkg * (bkg > limit)
+    icl_img_norm = icl_img / threshold # this version goes between 0 and 1
+    bkg = bkg * (1 - icl_img_norm) # fade out when the ICL is there. Might need this to be a mask instead
+    icl_img = bkg + icl_img
     # Add to the bright parts
     img_no_noise = (cutout * final_bright_parts) + icl_img
 
     # Generate some noise
-    std = mad_std(cutout * ~bright_parts) # Standard deviation of the background (+icl)
-    noise = np.random.normal(loc=0, scale=std, size=cutout.shape)
+    # std = mad_std(cutout * ~bright_parts) # Standard deviation of the background (+icl)
+    noise = np.random.normal(loc=0, scale=0.01, size=cutout.shape)
 
     # Add to final image
     img = img_no_noise + noise
     img = img.astype('<f4')
 
-    # Add to file
-    generated_data[f'{cutout_id}/HDU0/DATA'] = img
-    generated_data[f'{cutout_id}/FRAC'] = icl / total
-    generated_data[f'{cutout_id}/ICL'] = icl
-    generated_data[f'{cutout_id}/TOTAL'] = total
-    generated_data[f'{cutout_id}/PARAMS/AMP'] = amplitude
-    generated_data[f'{cutout_id}/PARAMS/ELLIP'] = ellip
-    generated_data[f'{cutout_id}/PARAMS/THETA'] = theta
-    generated_data[f'{cutout_id}/PARAMS/R_EFF'] = r_eff
-    
-    # plt.figure(figsize=(16,4))
-    # plt.subplot(141)
-    # plt.imshow(stretch(cutout))
-    # plt.xticks([])
-    # plt.yticks([])
-    # plt.title('Original image')
-    # plt.subplot(142)
-    # plt.imshow(stretch((cutout * bright_parts)))
-    # plt.xticks([])
-    # plt.yticks([])
-    # plt.title('Thresholded image')
-    # plt.subplot(143)
-    # plt.imshow(stretch(img_no_noise))
-    # plt.xticks([])
-    # plt.yticks([])
-    # plt.title('Added ICL profile')
-    # plt.subplot(144)
-    # plt.imshow(stretch(img))
-    # plt.xticks([])
-    # plt.yticks([])
-    # plt.title('Final image')
-    # # plt.suptitle(str(icl / total))
-    # plt.savefig('asdf.png')
-    
+    result = {}
+    result['img'] = img
+    result['icl'] = icl
+    result['total'] = total
+    result['amplitude'] = amplitude
+    result['ellip'] = ellip
+    result['theta'] = theta
+    result['r_eff'] = r_eff
+
+    return result
+
+if __name__ == '__main__':
+    cutouts = h5py.File('/srv/scratch/z5214005/lrg_cutouts_resized.hdf')
+    tbl = ascii.read('/srv/scratch/z5214005/lrgs_sampled.tbl')
+
+    # # Load the cluster member catalogue
+    # members = ascii.read('data/raw/camira_s20a_wide_member.dat', 
+    #                     names=['RA_cl', 'Dec_cl', 'Richness', 'z_cl', 'RA', 'Dec', 'M', 'w'])
+
+    # # Match this catalogue to the cluster catalogue
+    # merged = join(members, tbl, keys_left=['RA_cl', 'Dec_cl'], keys_right=['RA [deg]', 'Dec [deg]'])
+    # merged = merged['ID', 'Name', 'RA_cl', 'Dec_cl', 'z_cl_1', 'RA', 'Dec']
+
+    generated_data = h5py.File('/srv/scratch/mltidal/generated_data_otherlsb.hdf', 'w')
+    # fracs = []
+    # finder = SourceFinder(npixels=20, progress_bar=False, nlevels=8)
+
+    for num in range(len(tbl)):
+        print(f'{num}', end='\r')
+        cutout_id = tbl['new_ids'][num]
+        z = tbl['z'][tbl['new_ids'] == cutout_id]
+
+        result = inject_icl(cutout_id, cutouts, z)
+
+        if result is None:
+            continue
+
+        # Add to file
+        generated_data[f'{cutout_id}/HDU0/DATA'] = result['img']
+        generated_data[f'{cutout_id}/FRAC'] = result['icl'] / result['total']
+        generated_data[f'{cutout_id}/ICL'] = result['icl']
+        generated_data[f'{cutout_id}/TOTAL'] = result['total']
+        generated_data[f'{cutout_id}/PARAMS/AMP'] = result['amplitude']
+        generated_data[f'{cutout_id}/PARAMS/ELLIP'] = result['ellip']
+        generated_data[f'{cutout_id}/PARAMS/THETA'] = result['theta']
+        generated_data[f'{cutout_id}/PARAMS/R_EFF'] = result['r_eff']
